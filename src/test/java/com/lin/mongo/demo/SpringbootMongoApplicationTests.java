33f196d4cbc3fea3e8621d5ef9e9721623883ee2
linjie-033 <linjie-033@cpic.com.cn>
2022-06-24 16:46:57 +0800

    项目初始化

---
+++ src/test/java/com/lin/mongo/demo/SpringbootMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.mongo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
